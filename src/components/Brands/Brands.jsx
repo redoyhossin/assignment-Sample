bc2c426582115234e6632970fc7ca5009af5a932
redoyhossin <redoyhossin5984@gmail.com>
2024-11-20 18:49:41 +0600

    BrandAndCard

--- src/components/Brands/Brands.jsx
+++ src/components/Brands/Brands.jsx
@@ -1,11 +1,46 @@
-import React from 'react';
+import BrandsCard from "./BrandsCard/BrandsCard";
+
+import { useEffect, useState } from "react";
 
 const Brands = () => {
-    return (
-        <div>
-            
+  const [Brands, setBrands] = useState([]);
+  const [loading, setLoading] = useState(true); // State for loading
+
+  useEffect(() => {
+    // Fetch data
+    fetch("data.json") // URL
+      .then((response) => response.json())
+      .then((data) => {
+        setBrands(data[1]); // Set the fetched data
+        setLoading(false); // Set loading to false
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error); // Handle errors
+        setLoading(false);
+      });
+  }, []);
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+
+  return (
+    <div className="mx-auto max-w-screen-xl">
+
+<div className="text-center my-3 ">
+        <h2 className=" font-bold text-gray-800 md:text-3xl">Our Vision</h2>
+        <div className="inline-block my-7">
+          <hr className="bg-blue-900 h-[3px] w-24 " />
+          <hr className="bg-blue-900 h-[4px] w-16 ms-4 my-1" />
         </div>
-    );
+      </div>
+      <div className="grid grid-cols-1 gap-4 m-5 md:grid-cols-2 lg:grid-cols-2 lg:gap-8 justify-items-center">
+        {Brands.map((Brand) => (
+          <BrandsCard key={Brand.id} Brand={Brand}></BrandsCard>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Brands;
--- src/components/Brands/BrandsCard/BrandsCard.jsx
+++ src/components/Brands/BrandsCard/BrandsCard.jsx
@@ -1,11 +1,42 @@
 
-const BrandsCard = () => {
-   
-    return (
-        <div>
-     
+const BrandsCard = ({Brand}) => {
+  const { id, img, logoname, logotitle } = Brand;
+  
+  return (
+    <div>
+      <div className="   border border-blue-gray-50 bg-white  shadow-lg   transition hover:drop-shadow-2xl text-center ">
+        <div className="bg-[#0B60A90F] p-5" >
+          <img
+            className=""
+            src={img}
+            alt=""
+          />
+        </div>
+
+       <div className="px-5 pb-2">
+       <a href="#">
+          <h3 className="my-7 text-[22px] font-medium text-gray-900 hover:text-blue-800">
+            {logoname}
+          </h3>
+        </a>
+
+        <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
+          {logotitle}
+        </p>
+
+        <a
+          href="#"
+          className=" hover:text-blue-800 group mt-4 inline-flex items-center gap-1 text-base  font-semibold pb-4 text-gray-700 hover:text-[15px] transform hover:scale-95 transition-all duration-300 ease-in-out"
+        >
+          Know More
+          <span aria-hidden="true" className="">
+            &rarr;
+          </span>
+        </a>
+       </div>
+      </div>
     </div>
-    );
+  );
 };
 
 export default BrandsCard;
