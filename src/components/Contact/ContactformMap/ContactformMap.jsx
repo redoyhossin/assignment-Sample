11066fbb0decbac0d1a607369bf9aef0c270a3bf
redoyhossin <redoyhossin5984@gmail.com>
2024-11-24 15:28:30 +0600

    updatesometext

--- src/components/Contact/ContactformMap/ContactformMap.jsx
+++ src/components/Contact/ContactformMap/ContactformMap.jsx
@@ -3,16 +3,16 @@ const ContactformMap = () => {
   return (
     <div className="px-4 w-full">
       <div className="bg-white p-6">
-        <h4 className="text-2xl font-bold mb-2">BEOL HEAD OFFICE:</h4>
+        <h4 className="text-2xl font-bold mb-2">CORPORATE   HEAD OFFICE:</h4>
         <a href="tel:+880-2-58815319" className="underline font-bold my-2">
           +880-2-58815319
         </a>
         <p className="text-base font-bold my-3">
-          28 Gulshan North C/A, Gulshan Circle 2,Dhaka 1212, Bangladesh
+        Mostafa Centre, House 59, Road 27, Block K, Banani, Dhaka-1213
         </p>
         <div>
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3651.460281566795!2d90.46266107419015!3d23.766618488155768!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c9daf88fbc3f%3A0x88a1c67384927dd4!2sDasherkandi%20Aftabnagar%20Annenx%20Road!5e0!3m2!1sen!2sbd!4v1732310944119!5m2!1sen!2sbd"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2982.4487979666355!2d90.4062399572459!3d23.79451389792813!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c70051bf42cf%3A0xb7988b7486cd2182!2sMostafa%20Center!5e0!3m2!1sen!2sbd!4v1732438918493!5m2!1sen!2sbd"
             width="100%"
             height="330"
             // style="border:0;"
--- src/components/Contact/SharedMap/SharedMap.jsx
+++ src/components/Contact/SharedMap/SharedMap.jsx
@@ -4,7 +4,7 @@ const SharedMap = () => {
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-2 lg:gap-8 justify-items-center px-6">
         <div className="sm:hidden md:block lg:block w-full p-4  bg-white">
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d116840.10827635988!2d90.44031746416651!3d23.77398978799387!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sBangladesh%20Edible%20Oil%20Limited!5e0!3m2!1sen!2sbd!4v1732315751373!5m2!1sen!2sbd"
+            src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d3686.4637458107895!2d89.6002198741466!3d22.486777436112575!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1s5-8%2C%2011%20%26%2012%20Mongla%20EPZ%2C%20Bagerhat%20%20Mongla%20%20Bagerhat!5e0!3m2!1sen!2sbd!4v1732438682190!5m2!1sen!2sbd"
             width="100%"
             height="330"
             // style="border:0;"
