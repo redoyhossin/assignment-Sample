4459242a9bf72f725f2341d157805f2994e81da0
redoyhossin <redoyhossin5984@gmail.com>
2024-11-25 02:21:14 +0600

    logoimgnew

--- src/components/Navbars/Navbars.jsx
+++ src/components/Navbars/Navbars.jsx
@@ -322,7 +322,7 @@ const Navbars = () => {
           className="w-[150px] h-[75px]"
           
             loading="lazy"
-             src={'https://i.ibb.co.com/617mdHX/SGOil.png'}
+             src={'https://i.postimg.cc/3JxqqMBX/SGOil.png'}
             alt=""
           />
         </Link>
